ff7aec5c625c03f2ffea1dfd142163ac72e8decf
Guilherme Silva <silva.gsg@gmail.com>
2025-03-20 16:06:25 +0000

    feat: Add loading screen animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
+import "./index.css";
 import { LoadingScreen } from "./components/LoadingScreen";
+import { useState } from "react";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
-    <>
-      <LoadingScreen />
-    </>
+    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>
   );
 }
 
