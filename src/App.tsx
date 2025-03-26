66c5a1e17735f8fe23dbec5f1fa4568d01d213c5
Guilherme Silva <silva.gsg@gmail.com>
2025-03-26 13:07:58 +0000

    feat: Create and display nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@ import "./App.css";
 import "./index.css";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { useState } from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   return (
-    <>{!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}</>
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div
+        className={`min-h-screen transition-opacity duration-700 opacity-${
+          isLoaded ? "100" : "0"
+        } bg-black text-gray-100`}
+      >
+        <NavBar />
+      </div>
+    </>
   );
 }
 
