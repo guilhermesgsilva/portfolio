745ca9225213c83732eae9f2a8c1a08cf9c45632
Guilherme Silva <silva.gsg@gmail.com>
2025-07-14 22:25:08 +0100

    feat: Add navbar content and logic

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import NavBar from "./components/NavBar";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
@@ -14,7 +15,7 @@ function App() {
           isLoaded ? "100" : "0"
         } bg-black text-gray-100`}
       >
-        <NavBar />
+        <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       </div>
     </>
   );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,15 @@
-function NavBar() {
+import { useEffect } from "react";
+
+type NavBarProps = {
+  menuOpen: boolean;
+  setMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+function NavBar({ menuOpen, setMenuOpen }: NavBarProps) {
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? "hidden" : "";
+  }, []);
+
   return (
     <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
       <div className="max-w-5xl mx-auto px-4">
@@ -6,6 +17,40 @@ function NavBar() {
           <a href="#home" className="font-mono text-xl font-bold text-white">
             Guilherme<span className="text-blue-500">.tech</span>
           </a>
+          {/* Mobile */}
+          <div
+            className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
+            onClick={() => setMenuOpen((prev) => !prev)}
+          >
+            &#9776;
+          </div>
+          {/* Tablet & Desktop */}
+          <div className="hidden md:flex items-center space-x-8">
+            <a
+              href="#home"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              Home
+            </a>
+            <a
+              href="#about"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              About
+            </a>
+            <a
+              href="#projects"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              Projects
+            </a>
+            <a
+              href="#contact"
+              className="text-gray-300 hover:text-white transition-colors"
+            >
+              Contact
+            </a>
+          </div>
         </div>
       </div>
     </nav>
